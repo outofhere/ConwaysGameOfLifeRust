10b3f8e665191898447988163b883ec8ac9128bb
Igor Chervatyuk <ichervatyuk@gmail.com>
2020-07-30 16:42:39 +0300

    Working copy of CGOL

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,220 @@
-extern crate piston;
 extern crate glutin_window;
 extern crate graphics;
 extern crate opengl_graphics;
+extern crate piston;
 
-use piston::EventLoop;
-use piston::window::WindowSettings;
-use piston::event_loop::{Events, EventSettings};
-use piston::input::RenderEvent;
 use glutin_window::GlutinWindow;
-use opengl_graphics::{OpenGL, GlGraphics};
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::*;
+use piston::input::*;
+use piston::window::WindowSettings;
+use graphics::color;
 
-pub use crate::gameboard::Gameboard;
-pub use crate::gameboard_controller::GameboardController;
-pub use crate::gameboard_view::{GameboardView, GameboardViewSettings};
+const SIZEX: usize = 10;
+const SIZEY: usize = 10;
 
-mod gameboard;
-mod gameboard_controller;
-mod gameboard_view;
+#[derive(Clone)]
+enum CellState {
+    Alive,
+    Dead,
+}
+
+struct Game {
+    size_x: usize,
+    size_y: usize,
+    gl: GlGraphics,
+    seed: Vec<CellState>,
+    field: Vec<CellState>,
+}
+
+
+impl Game {
+    fn constructor(&mut self) {
+        self.seed = vec![CellState::Dead; self.size_x * self.size_y];
+        self.field = vec![CellState::Dead; self.size_x * self.size_y];
+        println!("Seed size: {}", self.seed.len());
+        println!("Field size: {}", self.field.len());
+    }
+
+    fn render(&mut self, args: &RenderArgs) {
+        self.gl.draw(args.viewport(), |_c, gl| {
+            graphics::clear(color::WHITE, gl);
+        });
+        for x in 0..self.size_x {
+            for y in 0..self.size_y {
+                match self.seed[x + y*self.size_x] {
+                    CellState::Alive => {
+                        let mut cell = Cell{pos_x: (x * 20) as u32, pos_y: (y * 20) as u32};
+                        cell.render(&mut self.gl, args);
+                        },
+                    CellState::Dead => {},
+                }
+            };
+        }
+    }
+
+    fn populate(&mut self, vec: Vec<Cell>) {
+        for item in vec {
+            if item.pos_x >= self.size_x as u32 || item.pos_y >= self.size_y  as u32 {
+                println!("[-] Invalid cell, will be ignored: {} {}", item.pos_x, item.pos_y);
+            } else {
+                println!("[+] Added: {} {}", item.pos_x, item.pos_y);
+                self.seed[item.pos_x as usize + self.size_x * item.pos_y as usize] = CellState::Alive;
+            }
+        }
+    }
+
+    fn play(&mut self){
+        let mut count_near_cells = 0;
+        for x in 0..self.size_x {
+            for y in 0..self.size_y {
+                let cells: Vec<(i32, i32)> = vec![
+                    (x as i32 - 1, y as i32 - 1),
+                    (x as i32 - 1, y as i32),
+                    (x as i32 - 1, y as i32 + 1),
+                    (x as i32, y as i32 - 1),
+                    (x as i32, y as i32 + 1),
+                    (x as i32 + 1, y as i32 - 1),
+                    (x as i32 + 1, y as i32),
+                    (x as i32 + 1, y as i32 + 1)
+                ];
+                for (x1, y1) in cells {
+                    if x1 < 0 || y1 < 0 { continue };
+                    if x1 as usize >= self.size_x || y1 as usize >= self.size_y { continue };
+                    match self.seed[(x1 as usize) + (y1 as usize) * self.size_x] {
+                        CellState::Alive => count_near_cells+= 1,
+                        CellState::Dead => {},
+                    }
+                }
+                if count_near_cells < 2 {
+                    self.field[x + y * self.size_x] = CellState::Dead;
+                } else if count_near_cells == 2 {
+                    match self.seed[x + y * self.size_x] {
+                        CellState::Alive =>  {
+                            self.field[x + y * self.size_x] = CellState::Alive;
+                            },
+                        CellState::Dead => {
+                            self.field[x + y * self.size_x] = CellState::Dead;
+                            },
+                        }
+                    } else if count_near_cells == 3 {
+                        self.field[x + y * self.size_x] = CellState::Alive;
+                        } else if count_near_cells > 3 {
+                            self.field[x + y * self.size_x] = CellState::Dead;
+                            } else {
+                                println!("Impossible situation {} {}", x, y);
+                                }
+                count_near_cells = 0;
+            }
+        }
+        self.seed = self.field.clone();
+    }
+}
+#[derive(Debug)]
+struct Cell {
+    pos_x: u32,
+    pos_y: u32,
+}
+
+impl Cell {
+    fn render(&mut self, gl: &mut GlGraphics, args: &RenderArgs) {
+        let square = graphics::rectangle::square(
+            self.pos_x as f64,
+            self.pos_y as f64,
+            20_f64,
+        );
+
+        gl.draw(args.viewport(), |c, gl| {
+            let transform = c.transform;
+            graphics::rectangle(color::BLACK, square, transform, gl);
+        });
+    }
+}
 
 fn main() {
     let opengl = OpenGL::V3_2;
-    let settings = WindowSettings::new("Conway's 'Game of Life' beta", [400; 2])
-        .graphics_api(opengl)
-        .exit_on_esc(true);
+    let mut window: GlutinWindow = WindowSettings::new(
+        "Convay",
+        [500; 2]
+    ).graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+    /*
+    let vec: Vec<Cell> = vec![
+        Cell{
+            pos_x: 1,
+            pos_y: 1,
+        },
+        Cell{
+            pos_x: 2,
+            pos_y: 1,
+        },
+        Cell{
+            pos_x: 1,
+            pos_y: 2,
+        },
+        Cell{
+            pos_x: 4,
+            pos_y: 4,
+        },
+        Cell{
+            pos_x: 4,
+            pos_y: 3,
+        },
+        Cell{
+            pos_x: 3,
+            pos_y: 4,
+        },
+    ];
+    */
+    let vec: Vec<Cell> = vec![
+        Cell{
+            pos_x: 3,
+            pos_y: 2,
+        },
+        Cell{
+            pos_x: 3,
+            pos_y: 3,
+        },
+        Cell{
+            pos_x: 3,
+            pos_y: 4,
+        },
+        Cell{
+            pos_x: 4,
+            pos_y: 1,
+        },
+        Cell{
+            pos_x: 4,
+            pos_y: 2,
+        },
+        Cell{
+            pos_x: 4,
+            pos_y: 3,
+        },
+    ];
 
-    let mut window: GlutinWindow = settings.build()
-        .expect("Could not create window");
 
-    let mut events = Events::new(EventSettings::new().lazy(true));
-    let mut gl = GlGraphics::new(opengl);
+    let mut game = Game {
+        size_x: SIZEX,
+        size_y: SIZEY,
+        gl: GlGraphics::new(opengl),
+        seed: Vec::new(),
+        field: Vec::new(),
+    };
 
-    let gameboard = Gameboard::new();
-    let mut gameboard_controller = GameboardController::new(gameboard);
-    let gameboard_view_settigns = GameboardViewSettings::new();
-    let gameboard_view = GameboardView::new(gameboard_view_settigns);
+    game.constructor();
+    game.populate(vec);
 
+    let mut events = Events::new(EventSettings::new()).ups(1);
     while let Some(e) = events.next(&mut window) {
-        gameboard_controller.event(&e);
         if let Some(args) = e.render_args() {
-            gl.draw(args.viewport(), |c, g| {
-                use graphics::{clear};
+            game.render(&args);
+        }
 
-                clear([1.0; 4], g);
-                gameboard_view.draw(&gameboard_controller, &c, g);
-            });
+        if let Some(_u) = e.update_args() {
+            game.play();
         }
     }
 }
